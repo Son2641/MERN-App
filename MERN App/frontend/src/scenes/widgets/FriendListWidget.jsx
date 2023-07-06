7a19f825355b7b3ff9fe0dc8dac9fa1e4e46cd63
Son2641 <jason.miranda@09gmail.com>
2023-07-07 00:32:37 +0800

    fix bugs

--- MERN App/frontend/src/scenes/widgets/FriendListWidget.jsx
+++ MERN App/frontend/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `https://connectson-api.onrender.com/${userId}/friends`,
+      `https://connectson-api.onrender.com/users/${userId}/friends`,
       {
         method: 'GET',
         headers: { Authorization: `Bearer ${token}` },
